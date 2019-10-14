bbc5a65869cdc639800f570d10197afb819877e0
firdausa7 <firdausa.salat@gmail.com>
2019-10-14 21:42:32 +0300

    add profile model

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    Name = models.TextField(default="Anonymous")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
+    bio = models.TextField(default="Welcome Me!")
