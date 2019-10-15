94d218beedfd2c18fcb7d1b92f158206ddca8260
firdausa7 <firdausa.salat@gmail.com>
2019-10-15 09:59:36 +0300

    add url paths for my account and edit

--- gram/urls.py
+++ gram/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^myaccount/$', views.mine, name='myaccount'),
+    url(r'^myaccount/edit/$', views.edit, name='edit'),
 ]
