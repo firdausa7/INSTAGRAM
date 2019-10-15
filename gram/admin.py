28ac389ae3f3deceaa60fa1c4cf529d914818ae7
firdausa7 <firdausa.salat@gmail.com>
2019-10-15 16:27:47 +0300

    update admin with models

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Comment,Post,Profile,Likes,Save,Follows
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Likes)
+admin.site.register(Save)
+admin.site.register(Follows)
